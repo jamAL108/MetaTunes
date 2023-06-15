57b23d8016cc343f96ff53eaa6e6c0af4af54da9
Jamal Mydeen <jamessies123@gmail.com>
2023-06-16 01:39:31 +0530

    Update allartist.js

--- client/src/components/allartist.js
+++ client/src/components/allartist.js
@@ -1,5 +1,6 @@
 import React, { useEffect , useState } from 'react'
 import './allartist.css';
+
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getallartist } from '../redux/action/useraction';
