094dc0066a127fcc4bcdf03d5ebffc23b9613eea
JamalMydeen <jamessies123@gmail.com>
2023-06-18 19:59:54 +0530

    ui

--- client/src/pages/addplaylist.js
+++ client/src/pages/addplaylist.js
@@ -6,6 +6,8 @@ import Nav from '../components/nav';
 import AddIcon from '@mui/icons-material/Add';
 import '../css/addplaylist.css';
 import $ from 'jquery';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 const Addplaylist = () => {
     const user = JSON.parse(localStorage.getItem("user"));
     const navigate = useNavigate();
@@ -18,6 +20,7 @@ const Addplaylist = () => {
       name:"",
       description:""
     })
+    const [selected , setselected]=useState([]);
     useEffect(()=>{
        if(!user){
         toast.warn("PLease login to create playlists", {
@@ -59,6 +62,51 @@ const Addplaylist = () => {
       console.log("helo");
       $("#imageUpload").click();
   });
+
+  const [songlist,setsonglist]=useState([]);
+  useEffect(()=>{
+     const song = JSON.parse(localStorage.getItem("song"));
+     if(song){
+      let array=[];
+      for(var i=1;i<=5;i++){
+      const randomsong = song[(Math.floor(Math.random() * (song.length)))];
+         array.push(randomsong);
+         console.log("hey");
+      }
+      setsonglist(array);
+      showBoxes();
+     }
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  const refresh =()=>{
+    const song = JSON.parse(localStorage.getItem("song"));
+    if(song){
+      let array=[];
+      while(array.length!==5){
+      const randomsong = song[(Math.floor(Math.random() * (song.length)))];
+      if(!selected.includes(randomsong)){
+         array.push(randomsong);
+      }
+      }
+      const boxes = document.getElementsByClassName(".box");
+      for(var i=0;i<boxes.length;i++){
+        console.log("heyy");
+        boxes[i].style.opacity="0";
+      }
+      setsonglist(array);
+      showBoxes();
+    }
+  }
+  const boxes = document.getElementsByClassName(".box");
+  // console.log(boxes);
+  function showBoxes() {
+    for (let i = 0; i < boxes.length; i++) {
+      const box = boxes[i];
+      box.style.animationDelay = `${i * 0.5}s`; // Set animation delay based on index
+    }
+  }
+showBoxes();
+
   return (
     <div className="addplaylist">
         <ToastContainer/>
@@ -113,13 +161,47 @@ const Addplaylist = () => {
                           }} />
                         </div>
                         <div className="item1">
-                          <label htmlFor="input1">Description (optional)</label>
+                          <label htmlFor="input1">Playlist Description (optional)</label>
                           <textarea className='para' value={info.description} maxLength={100} cols="4" rows="4" onChange={(e)=>{
                             setinfo({...info , description:e.target.value})
                           }} />
                         </div>
                        </div>
             </form>
+            <div className="songadd">
+                 <div className="head">
+                  <h1>Add song</h1>
+                  <button onClick={(e)=>{
+                    e.preventDefault();
+                    refresh();
+                  }} >Refresh<RefreshIcon/></button>
+                 </div>
+                 <div className="lineeeeey"></div>
+                 <div className="songlist">
+                 {songlist.map((item,idx)=>(
+                           <div className="box" key={idx} >
+                           <div className="left">
+                           <div className="image">
+                           <img src={item.imageURL} alt="pic" />
+                           </div>
+                           <div className="names">
+                             <h2>{item.name}</h2>
+                             <p>{item.artist}</p>
+                           </div>
+                         </div>
+                         <div className="right">
+                           <AddCircleOutlineOutlinedIcon className='add' onClick={(e)=>{
+                            e.preventDefault();
+                            let array = [...selected];
+                            array.push(item);
+                            setselected(array);
+
+                           }}/>
+                         </div>
+                         </div>
+                ))}
+                 </div>
+            </div>
         </div>
     </div>
   )
